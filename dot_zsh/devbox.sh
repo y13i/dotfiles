bd51bb66ec083e0f0e826811baf9da34befd23e5
Yoriki Honda <7214104+y13i@users.noreply.github.com>
2026-07-21 21:15:10 +0900

    Update devbox.sh

--- dot_zsh/devbox.sh
+++ dot_zsh/devbox.sh
@@ -1,1 +1,3 @@
 if command -v devbox &>/dev/null; then eval "$(devbox global shellenv)"; fi
+
+[[ -f "${HOME}/.oh-my-zsh/completions/_devbox" ]] || devbox completion zsh > ~/.oh-my-zsh/completions/_devbox
