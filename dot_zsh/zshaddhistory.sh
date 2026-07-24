0ccd181ac30987e1279614a6c863336dda7ad1fa
Yoriki Honda <7214104+y13i@users.noreply.github.com>
2026-07-24 10:33:10 +0900

    Update zshaddhistory.sh

--- dot_zsh/zshaddhistory.sh
+++ dot_zsh/zshaddhistory.sh
@@ -1,6 +1,7 @@
 function zshaddhistory() {
   emulate -L zsh
-  if [[ $1 = *"AWS_SECRET_ACCESS_KEY"* ]] ; then
+  setopt local_options extended_glob
+  if [[ $1 = *(#i)PASSWORD* || $1 = *"AWS_SECRET_ACCESS_KEY"* ]] ; then
     return 1
   fi
   return 0
