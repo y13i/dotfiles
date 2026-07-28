a8cae6c5f33177e5c472756eb3074cfe6f34c89a
Yoriki Honda <7214104+y13i@users.noreply.github.com>
2026-07-28 15:20:45 +0900

    Add eza aliases

--- dot_zsh/aliases.sh
+++ dot_zsh/aliases.sh
@@ -34,7 +34,3 @@ alias asg="aws sts get-caller-identity"
 alias digs="dig +short"
 
 alias lf="less +F"
-
-alias e="eza"
-alias el="eza -l"
-alias ela="eza -la"
--- dot_zsh/eza.sh
+++ dot_zsh/eza.sh
@@ -1,5 +1,7 @@
 alias e="eza"
 alias el="eza -l"
 alias ela="eza -la"
+alias et="eza --tree"
 
 alias ls="eza"
+alias lt="eza --tree"
